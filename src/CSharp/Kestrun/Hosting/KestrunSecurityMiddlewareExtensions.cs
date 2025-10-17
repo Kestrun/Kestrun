6715c0862af95e2b15974fbacc07229fc29e7ea6
mdaneri <m_daneri@hotmail.com>
2025-10-16 21:37:32 -0700

    Update src/CSharp/Kestrun/Hosting/KestrunSecurityMiddlewareExtensions.cs
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>

--- src/CSharp/Kestrun/Hosting/KestrunSecurityMiddlewareExtensions.cs
+++ src/CSharp/Kestrun/Hosting/KestrunSecurityMiddlewareExtensions.cs
@@ -12,7 +12,6 @@ namespace Kestrun.Hosting;
 /// </summary>
 public static class KestrunSecurityMiddlewareExtensions
 {
-
     /// <summary>
     /// Adds rate limiting to the application using the specified <see cref="RateLimiterOptions"/>.
     /// </summary>
